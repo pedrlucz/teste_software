12e784db349c786ab350223a2872b1af8de88c41
pedrlucz <pedrlucz7@gmail.com>
2025-05-25 02:52:46 -0300

    create url for updated/delete

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from views import Users
+from .views import Users
 
 urlpatterns = [
-    path('users/', Users.as_view(), name = 'usuarios')
+    path('users/', Users.as_view(), name = 'usuarios'),
+    path('users/<int:id>/', Users.as_view(), name = 'update-user')
 ]
