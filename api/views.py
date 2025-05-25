4251671950809021d08f194e6c572c442a2d0e01
pedrlucz <pedrlucz7@gmail.com>
2025-05-25 02:51:11 -0300

    fixing

--- api/views.py
+++ api/views.py
@@ -1,16 +1,24 @@
+from rest_framework import generics, mixins, status
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .serializers import UserSerializer
 from django.http import HttpResponse
-from rest_framework import status
 from .models import User
-import requests, os
 
-class Users(APIView):
+class Users(mixins.ListModelMixin, mixins.RetrieveModelMixin, generics.GenericAPIView):
+    """"""
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    lookup_field = 'id'
+
     def post(self, request):
-        name = request.get('name')
-        email = request.get('email')
-        cpf = request.get('cpf')
+        payload = request.data
+
+        name = payload.get('name')
+        email = payload.get('email')
+        cpf = payload.get('cpf')
 
         try:
              User.objects.create(
@@ -22,40 +30,48 @@ class Users(APIView):
         except Exception as e:
             return Response({'saved': f'Não foi possível salvar no banco de dados? {e}'}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        return Response(status = 200)
+        return Response('Novo usuário salvo!', status = 200)
 
-    def put(self, request):
-        phone = request.get('phone')
-        age = request.get('age')
+    def put(self, request, id):
+        payload = request.data
 
-        try:
-             User.objects.create(
-                phone = phone,
-                age = age
-            )
+        phone = payload.get('phone')
+        age = payload.get('age')
+
+        user = get_object_or_404(User, id = id)
+
+        try: 
+            user.phone = phone
+            user.age = age 
+
+            user.save(update_fields=['phone', 'age'])
 
         except Exception as e:
             return Response({'saved': f'Não foi possível salvar no banco de dados? {e}'}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        return Response(status = 200)
+        return Response('Usuário atualizado!', status = 200)
 
     def get(self, request, id = None):
         if id is not None:
-            return self.retrieve(request)
+            return self.retrieve(request, id = id)
 
         return self.list(request)
 
     def delete(self, request, id):
+        payload = request.data
+
         user = get_object_or_404(User, id = id)
+        user.delete()
 
-        email = request.get('email')
-        phone = request.get('phone')
-        age = request.get('age')
-        name = request.get('name')
+        # depois ver se consigo fazer
+        # email = payload.get('email')
+        # phone = payload.get('phone')
+        # age = payload.get('age')
+        # name = payload.get('name')
 
-        for field, value in (('email', email), ('phone', phone), ('age', age), ('name', name)):
-            setattr(user, field, value)
+        # for field, value in (('email', email), ('phone', phone), ('age', age), ('name', name)):
+        #     setattr(user, field, value)
 
-        user.save(update_fields=['email', 'phone_number', 'age', 'name'])
+        # user.save(update_fields=['email', 'phone_number', 'age', 'name'])
 
         return Response(status = 200)
